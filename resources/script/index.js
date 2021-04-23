f7e2514928456de3920c3e68ae9a68871dc19cbe
nardos111 <nardos2alemu@gmail.com>
2021-04-23 03:15:57 +0300

    Add Advice Slip JSON API

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,1 +1,12 @@
+const API_URL = "https://api.adviceslip.com/advice";
+function get(url) { return fetch(url).then(resp => resp.json()) }
+const API = { get }
 
+function getQuotes() {
+    API.get(API_URL).then(data => addQuote(data['slip']['advice']))
+}
+
+function addQuote(quote) {
+    para = document.getElementById("quote")
+    para.innerText = quote;
+}
