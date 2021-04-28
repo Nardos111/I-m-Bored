61e681a67d96ae8c3bf9010178125cedcde74935
nardos111 <nardos2alemu@gmail.com>
2021-04-28 04:19:21 +0300

    Add joke api

--- resources/script/index.js
+++ resources/script/index.js
@@ -1,12 +1,36 @@
 const API_URL = "https://api.adviceslip.com/advice";
 function get(url) { return fetch(url).then(resp => resp.json()) }
 const API = { get }
+const jokepara = document.getElementById("joke");
+const buttonClicked = document.getElementById("jokes");
+const url = "https://icanhazdadjoke.com/"
+
 
 function getQuotes() {
     API.get(API_URL).then(data => addQuote(data['slip']['advice']))
 }
 
 function addQuote(quote) {
-    para = document.getElementById("quote")
+    const para = document.getElementById("quote")
     para.innerText = quote;
 }
+
+
+
+const options = {
+    method: 'GET',
+    headers: {
+        "Accept": "application/json"
+    }
+};
+
+buttonClicked.onclick = function () {
+    fetch("https://icanhazdadjoke.com/", options)
+        .then((res) => {
+            return res.json();
+        }).then((data) => {
+            jokepara.innerText = data.joke;
+        });
+};
+
+
