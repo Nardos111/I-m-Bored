a05753efca9dec36265db1c7e8351074ed972a0d
nardos111 <nardos2alemu@gmail.com>
2021-04-29 23:28:45 +0300

    Improve advice page design

--- resources/script/advice.js
+++ resources/script/advice.js
@@ -2,12 +2,22 @@
 const API_URL = "https://api.adviceslip.com/advice";
 function get(url) { return fetch(url).then(resp => resp.json()) }
 const API = { get }
+const para = document.getElementById("quote")
+const loadAdvice = document.getElementById("reload")
 
-function getQuotes() {
-    API.get(API_URL).then(data => addQuote(data['slip']['advice']))
-}
 
-function addQuote(quote) {
-    const para = document.getElementById("quote")
-    para.innerText = quote;
-}
+const options = {
+    method: 'GET',
+    headers: {
+        "Accept": "application/json"
+    }
+};
+
+loadAdvice.onclick = function () {
+    fetch(API_URL, options)
+        .then((res) => {
+            return res.json();
+        }).then((data) => {
+            para.innerText = data['slip']['advice'];
+        });
+};
